27094f222083bf594c100f25409b797f118f6f27
DiusFirius <dm.aristovv@gmail.com>
2025-05-27 08:29:50 +0200

    [PlanktonRipper] Interim non working patch

---
+++ PlanktonRipper/Main.cs
@@ -0,0 +1,69 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DiusFirius.PlanktonRipper
+{
+    [QModCore]
+    public class Main
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            LogMessage("Starting PlanktonRipper patching...");
+
+            var harmony = new Harmony("com.diusfirius.planktonripper");
+            harmony.PatchAll();
+
+            LogMessage("Patched successfully!");
+        }
+
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(ReikaKalseki.Ecocean.BaseCellEnviroHandler))]
+        //[HarmonyPatch("ReikaKalseki.Ecocean.BaseCellEnviroHandler.depth")]
+        //private static bool PatchComputeEnvironment(ref int __result)
+        //{
+        //    LogMessage("Triggered patch");
+        //    __result = 1;
+        //    return false;
+        //}
+
+        //[HarmonyPrefix]
+        ////[HarmonyPatch(typeof(ReikaKalseki.Ecocean.BaseCellEnviroHandler))]
+        ////[HarmonyPatch("ReikaKalseki.Ecocean.BaseCellEnviroHandler.computeEnvironment")]
+        //[HarmonyPatch(typeof(ReikaKalseki.Ecocean.BaseCellEnviroHandler), "computeEnvironment")]
+        //[HarmonyPriority(Priority.Last)]
+        //private static bool PatchComputeEnvironmentAnother(ref bool __result)
+        //{
+        //    LogMessage("Triggered patch new");
+        //    __result = false;
+        //    return false;
+        //}
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ReikaKalseki.Ecocean.BaseCellEnviroHandler), "Update")]
+        [HarmonyPriority(Priority.Last)]
+        private static void AlwaysDestroyPlanktonPostfix(ReikaKalseki.Ecocean.BaseCellEnviroHandler __instance)
+        {
+            var field = AccessTools.Field(typeof(ReikaKalseki.Ecocean.BaseCellEnviroHandler), "plankton");
+            GameObject plankton = (GameObject)field.GetValue(__instance);
+
+            if (plankton != null)
+            {
+                UnityEngine.Object.Destroy(plankton);
+                field.SetValue(__instance, null);
+                LogMessage("Plankton destroyed successfully.");
+            }
+        }
+
+        private static void LogMessage(string message)
+        {
+            UnityEngine.Debug.Log($"[PlanktonRipper] {message}");
+        }
+    }
+}
---
+++ PlanktonRipper/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PlanktonRipper")]
+[assembly: AssemblyDescription("Does not allow plankton spawn near player base (Ecocean mod for subnautica)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DiusFirius")]
+[assembly: AssemblyProduct("PlanktonRipper")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1616f226-1da0-455e-8913-e98efbeec8cd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
